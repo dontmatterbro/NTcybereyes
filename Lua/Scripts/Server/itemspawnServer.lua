db706b0b63905ba8f44c33e2730e35d2e82682ac
dontmatterbro <yh846175@gmail.com>
2024-08-03 17:41:44 +0300

    itemspawn

--- Lua/Scripts/Server/itemspawnServer.lua
+++ Lua/Scripts/Server/itemspawnServer.lua
@@ -3,7 +3,6 @@
 Hook.Add("roundStart", "spawninfrareditem", function()
 local HUDitemprefab = ItemPrefab.GetItemPrefab("eyethermalHUDitem")
 local HUDitemposition = Vector2(1, 1)
-Entity.Spawner.AddItemToSpawnQueue(HUDitemprefab, HUDitemposition, nil, nil, function(item) end)
-print("roundstartspawn")
+Entity.Spawner.AddItemToSpawnQueue(HUDitemprefab, Submarine.MainSub, Submarine.MainSub, nil, function(item) end)
 end)
 
